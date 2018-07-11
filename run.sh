6f65802f0e6d036b5fc87d3ac7041bc06b88f5b9
Engin Kayraklioglu <ekayrakli@gmail.com>
2018-07-11 19:03:48 -0400

    Make the random number generator random

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./bin/test_release --learning_rate 0.7 --epochs 30 --minibatch_size 16 --backend_type internal < pipelog
+./bin/test_release --learning_rate 0.7 --epochs 30 --minibatch_size 16 --backend_type internal < sample_input
