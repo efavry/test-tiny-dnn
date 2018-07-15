4c7a75cbb59f59b9d2dcf7453c53d06f046360dc
Engin Kayraklioglu <ekayrakli@gmail.com>
2018-07-15 16:48:52 -0400

    Add the environment setter for tinydnn

---
+++ setenv.sh
@@ -0,0 +1,2 @@
+export PATH=/home/engin/builds/gcc/gcc-6.3.0-obj-pyr/bin:$PATH
+export LD_LIBRARY_PATH=/home/engin/builds/gcc/gcc-6.3.0-obj-pyr/lib:/home/engin/builds/gcc/gcc-6.3.0-obj-pyr/lib64:/home/engin/builds/isl/isl-0.16.1-obj-pyr/lib:/home/engin/builds/mpc/mpc-1.0.3-obj-pyr/lib:/home/engin/builds/mpfr/mpfr-3.1.4-obj-pyr/lib:/home/engin/builds/gmp/gmp-6.1.0-obj-pyr/lib:$LD_LIBRARY_PATH
